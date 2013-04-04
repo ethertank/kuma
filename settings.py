7db8dec9edcf8ccdc838ac11b166656af6677422
groovecoder <luke.crouch@gmail.com>
2013-04-04 09:33:49 -0500

    change mdn_prod logging to admin emails

--- settings.py
+++ settings.py
@@ -1108,9 +1108,9 @@ LOGGING = {
             'level': LOG_LEVEL,
         },
         'mdn_prod': {
-            'class': 'logging.handlers.SysLogHandler',
+            'class': 'django.utils.log.AdminEmailHandler',
             'formatter': 'mdn_default',
-            'level': LOG_LEVEL,
+            'level': logging.ERROR,
         },
     },
     'loggers': {
@@ -1119,6 +1119,5 @@ LOGGING = {
             'level': LOG_LEVEL,
             'propagate': False,
         },
-        
     },
 }
